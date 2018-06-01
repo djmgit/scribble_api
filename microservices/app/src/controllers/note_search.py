abf85594e324f9c9088c39df7c68d717cd184fca
djmgit <djmdeveloper060796@gmail.com>
2018-06-01 13:21:06 +0530

    Updated ms

---
+++ microservices/app/src/controllers/note_search.py
@@ -0,0 +1,41 @@
+import datetime
+from flask import Blueprint, jsonify, request
+from flask import current_app as app
+import json
+import requests
+from helpers.check_login import is_loggedin
+from helpers.search_notes import search_notes
+
+router = Blueprint('note_search', __name__)
+
+@router.route('/note_search', methods=['POST'])
+def add_note():
+	response = ""
+
+	try:
+		data = request.get_json()
+	except Exception as e:
+		response = {"status": "data not found"}
+		return jsonify(response)
+
+	auth_token = request.headers.get('Authorization')
+	if not auth_token or auth_token == "":
+		response = {"status": "auth token not specified"}
+		return jsonify(response)
+
+	hasura_id = is_loggedin(auth_token)
+	if not hasura_id:
+		response = {"status": "User is not logged in. Please login first"}
+		return jsonify(response)
+
+	search_data = data.get("search")
+	if not search_data:
+		response = {"status": "Search data is not specified"}
+		return jsonify(response)
+
+	fields = data.get("fields")
+	if not fields:
+		fields = ['note_title', 'note_body', 'keywords', 'category']
+
+	response = search_notes(hasura_id, search_data, fields)
+	return jsonify(response)

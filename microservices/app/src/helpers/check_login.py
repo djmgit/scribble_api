102f98f27141e3714737f94765d9499bf5ec157a
djmgit <djmdeveloper060796@gmail.com>
2018-06-01 16:22:26 +0530

    Fixed bug

--- microservices/app/src/helpers/check_login.py
+++ microservices/app/src/helpers/check_login.py
@@ -6,7 +6,7 @@ def is_loggedin(token):
 
 	headers = {
     	"Content-Type": "application/json",
-    	"Authorization": "Bearer {}".format(token)
+    	"Authorization": token
 	}
 
 	resp = requests.request("GET", url, headers=headers)
